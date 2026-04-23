c669ea98654075a039c713185cffa7c7ffd6f399
Ratchmartins <osykmartins.com>
2026-04-23 13:02:50 +0000

    Deploying CRUD App

--- app/app.py
+++ app/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
+import socket
 
 app = Flask(__name__)
 
-# Secure DB Configuration
+# DB Config
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASS = os.getenv('DB_PASS', 'password')
 DB_HOST = os.getenv('DB_HOST', 'db-service')
 DB_NAME = os.getenv('DB_NAME', 'myapp_db')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -21,16 +21,34 @@ class Item(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/items', methods=['POST', 'GET'])
-def manage_items():
+# --- ROUTES ---
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        new_item = Item(name=request.json['name'])
+        new_item = Item(name=request.form['name'])
         db.session.add(new_item)
         db.session.commit()
-        return jsonify({"status": "created"}), 201
+        return redirect(url_for('index'))
+    
     items = Item.query.all()
-    return jsonify([{"id": i.id, "name": i.name} for i in items])
+    return render_template('index.html', items=items, pod=socket.gethostname())
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    item = Item.query.get_or_404(id)
+    if request.method == 'POST':
+        item.name = request.form['name']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', item=item)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # Fixed for Bandit B104
-    app.run(host=os.getenv('APP_HOST', '0.0.0.0'), port=5000) # nosec B104
+    app.run(host='0.0.0.0', port=5000) # nosec B104
