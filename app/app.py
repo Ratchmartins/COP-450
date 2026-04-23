f719c985513d494c1e877207a067055f928a969d
Ratchmartins <osykmartins.com>
2026-04-23 12:23:54 +0000

    Deploying CRUD App

--- app/app.py
+++ app/app.py
@@ -4,16 +4,16 @@ import os
 
 app = Flask(__name__)
 
-# Fetch database credentials from Environment Variables
+# Secure DB Configuration
 DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASS = os.getenv('DB_PASS', 'password')
 DB_HOST = os.getenv('DB_HOST', 'db-service')
 DB_NAME = os.getenv('DB_NAME', 'myapp_db')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Database Model
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -21,18 +21,16 @@ class Item(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/items', methods=['POST'])
-def create_item():
-    data = request.json
-    new_item = Item(name=data['name'])
-    db.session.add(new_item)
-    db.session.commit()
-    return jsonify({"message": "Item created"}), 201
-
-@app.route('/items', methods=['GET'])
-def get_items():
+@app.route('/items', methods=['POST', 'GET'])
+def manage_items():
+    if request.method == 'POST':
+        new_item = Item(name=request.json['name'])
+        db.session.add(new_item)
+        db.session.commit()
+        return jsonify({"status": "created"}), 201
     items = Item.query.all()
     return jsonify([{"id": i.id, "name": i.name} for i in items])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) # nosec B104
+    # Fixed for Bandit B104
+    app.run(host=os.getenv('APP_HOST', '0.0.0.0'), port=5000) # nosec B104
